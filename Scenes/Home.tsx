5006ccbe63a1fe4aa4acff7190451d424c907bf1
shinp <shinpeiyoshihara@gmail.com>
2018-10-22 08:24:16 +0900

    aa

--- Scenes/Home.tsx
+++ Scenes/Home.tsx
@@ -8,21 +8,65 @@
 
 import React, { Component, ReactNode } from 'react';
 import { Platform, StyleSheet, Text, View, Button } from 'react-native';
-import { 
+import {
   NavigationEvents,
   NavigationContainer,
-  NavigationScreenProp } from 'react-navigation';
+  NavigationScreenProp
+} from 'react-navigation';
 
 class NavComponent extends Component<{
-  navigation: NavigationScreenProp<any,any>
+  navigation: NavigationScreenProp<any, any>
 }>{
 
 }
 
 export default class Home extends NavComponent {
+  componentDidMount() {
+    fetch("http://google.co.jp").then(
+      ($e: Response) => {
+        alert('success!')
+      },
+      () => alert("Error!")
+    )
+  }
+
   render(): JSX.Element {
     return (
-      <View style={styles.container}>
+      <View style={
+        {
+          flex: 1,
+          justifyContent: 'flex-start',
+          alignItems: 'flex-end',
+          backgroundColor: '#F5FCFF',
+        }
+      }>
+        <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
+        <View style={{ flexDirection: "row" }} >
+          <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
+          <View style={{ width: 50, height: 50, backgroundColor: 'yellow' }} />
+          <View style={{ width: 50, height: 50, backgroundColor: 'blue' }} />
+          <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
+          <View style={{ width: 50, height: 50, backgroundColor: 'yellow' }} />
+          <View style={{ width: 50, height: 50, backgroundColor: 'blue' }} />
+          <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
+          <View style={{ width: 50, height: 50, backgroundColor: 'yellow' }} />
+          <View style={{ width: 50, height: 50, backgroundColor: 'blue' }} />
+
+        </View>
+        <Text>
+
+          aaaa
+        </Text>
+        <View style={{ flexDirection: 'column' }} >
+          <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
+          <View style={{ width: 50, height: 50, backgroundColor: 'yellow' }} />
+          <View style={{ width: 50, height: 50, backgroundColor: 'blue' }} />
+          <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
+          <View style={{ width: 50, height: 50, backgroundColor: 'yellow' }} />
+          <View style={{ width: 50, height: 50, backgroundColor: 'blue' }} />
+
+        </View>
+
         <Text style={styles.welcome}>
           Home
         </Text>
@@ -30,7 +74,7 @@ export default class Home extends NavComponent {
           title="test ffff"
           onPress={
             () => {
-              this.props.navigation.navigate("Profile",{});
+              this.props.navigation.navigate("Profile", {});
               alert(123);
 
             }
@@ -44,12 +88,6 @@ export default class Home extends NavComponent {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
